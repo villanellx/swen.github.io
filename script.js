7ea4eeed8bac245a6919be76a34c688617987cfa
Villanelle <92764960+villanellx@users.noreply.github.com>
2022-01-23 16:23:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var palavras = ["A Viagem", "The Nanny", "Vamp", "Pica Pau", "Era Uma Vez", "Corpse Bride", "Up Close & Personal", "Os Simpsons", "A Favortia"]
+var palavras = ["A Viagem", "The Nanny", "Vamp", "Pica Pau", "Era Uma Vez", "Corpse Bride", "Up Close & Personal", "Os Simpsons", "A Favorita"]
 let button = document.getElementById('sortear').addEventListener('click', sorteio)
 
 function sorteio() {
